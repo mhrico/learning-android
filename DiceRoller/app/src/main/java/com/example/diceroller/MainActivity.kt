1a248b075780ea97f27d849cad9118008ff35496
Mehedi Hassan Rico <mhrico2@gmail.com>
2021-12-14 19:26:22 +0600

    feat: Roll 2 dice

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice1(); rollDice2() }
+    }
+
+    private fun getRandomDiceImage() : Int {
+        return (1..6).random()
+    }
+
+    private fun rollDice1() {
+        val drawableResource = when (getRandomDiceImage()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage1.setImageResource(drawableResource)
+    }
+
+    private fun rollDice2() {
+        val drawableResource = when (getRandomDiceImage()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage2.setImageResource(drawableResource)
+    }
+}
